2b399dfaf5026df415cec68a9e113944b780884f
mj <mj@home.lab>
2023-02-25 18:25:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Argos Extensions
+Argos is gnome extension for making other simple extensions by utilizing bash scripts. This repository contains my scripts for argos.
+
+## Installation
+1. Install and enable [argos](https://github.com/p-e-w/argos) gnome extension.
+2. Create ~/.config/argos directory and place your scripts there.
+3. You may need to logoff/login.
---
+++ todo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 2023-02-25
+# Martynas J.
+# https://github.com/f5AFfMhv
+#
+# Argos script for simple todo plugin. Plugin displays ~/Documents/todo file content.
+ENTRY_COUNT=$(cat ~/Documents/todo | wc -l)
+echo "($ENTRY_COUNT) | iconName=emblem-documents"
+echo "---"
+cat ~/Documents/todo
+echo "---"
+echo "EDIT | bash='gnome-text-editor ~/Documents/todo' iconName=media-floppy terminal=false refresh=true"
+echo "---"
